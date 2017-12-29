237a1f77f8e77eb49ee53dcd56ebeed5a9af51c8
jorgegvalencia <jorgegvalencia@gmail.com>
2017-12-29 17:35:13 +0100

    Update styles references and build task

--- gulp/tasks/dist-images.js
+++ gulp/tasks/dist-images.js
@@ -1,13 +1,15 @@
 (function () {
   module.exports = function (gulp, $, config, args) {
     return function () {
-      return gulp.src([config.paths.images])
+      return gulp.src([config.paths.images], {
+          base: config.paths.src
+        })
         .pipe($.cache($.imagemin({
           optimizationLevel: 3,
           progressive: true,
           interlaced: true
         })))
-        .pipe(gulp.dest(config.paths.dist + '/images'))
+        .pipe(gulp.dest(config.paths.dist))
         .pipe($.size());
     };
   };
--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@
 
   // Tasks definition
   gulp.task('dist-clean', task('dist-clean'));
-  gulp.task('dist-build', gulpsync.sync(['sass', 'dist-clean', 'dist-fonts'], 'dist-build'), task('dist-build'));
+  gulp.task('dist-build', gulpsync.sync(['sass', 'dist-clean', 'dist-fonts', 'dist-images'], 'dist-build'), task('dist-build'));
   gulp.task('dist-images', task('dist-images'));
   gulp.task('dist-fonts', task('dist-fonts'));
   gulp.task('sass', task('sass'));
