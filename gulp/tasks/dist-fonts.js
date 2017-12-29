b6d7098b5a93cb7e52102c1c4a93f88689d8e9b0
jorgegvalencia <jorgegvalencia@gmail.com>
2017-12-30 00:48:28 +0100

    Update sources and fonts directory

--- gulp/tasks/dist-fonts.js
+++ gulp/tasks/dist-fonts.js
@@ -2,7 +2,7 @@
   module.exports = function (gulp, $, config, args) {
     return function () {
       return gulp.src([config.paths.bootstrap.fonts])
-        .pipe(gulp.dest(config.paths.dist + '/fonts/'));
+        .pipe(gulp.dest(config.paths.dist + '/assets/fonts/'));
     };
   };
 }());
